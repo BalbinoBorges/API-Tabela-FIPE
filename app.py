2eeee6a2d89c2769d51f6f5ac13e584644ac4abf
BalbinoBorges <114811873+BalbinoBorges@users.noreply.github.com>
2023-05-16 15:23:08 -0300

    app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/fipe', methods=['GET'])
+def obter_dados_fipe():
+    marca = request.args.get('marca')
+    modelo = request.args.get('modelo')
+    ano = request.args.get('ano')
+
+    url = f"https://veiculos.fipe.org.br/api/veiculos/ConsultarValorComTodosParametros?codigoTabelaReferencia=277&codigoMarca={marca}&codigoModelo={modelo}&anoModelo={ano}&codigoTipoCombustivel=1&tipoVeiculo=carro&tipoConsulta=tradicional"
+    headers = {
+        'Referer': 'https://veiculos.fipe.org.br/'
+    }
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    # Suporte ao formato JSONP para uso no Power BI
+    callback = request.args.get('callback')
+    if callback:
+        return f"{callback}({jsonify(data).data.decode('utf-8')})"
+    else:
+        return jsonify(data)
+
+if __name__ == '__main__':
+    app.run()
